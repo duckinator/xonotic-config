a14e804af7997e00f21668a2f51a1bdf8e113129
Ellen Marie Dash <me@duckie.co>
2020-10-18 19:39:27 -0400

    initial commit

---
+++ server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Set $XONOTIC_DEDICATED to override.
+XONOTIC=""
+
+if [ -n "$XONOTIC_DEDICATED" ]; then
+    XONOTIC="$XONOTIC_DEDICATED"
+elif which xonotic-linux64-dedicated &>/dev/null; then
+    XONOTIC=xonotic-linux64-dedicated
+elif which xonotic-dedicated &>/dev/null; then
+    XONOTIC=xonotic-dedicated
+else
+    XONOTIC=./Xonotic/xonotic-linux64-dedicated
+fi
+
+mkdir -p userdir/data || exit 1
+cp server/server.cfg userdir/data/server.cfg || exit 1
+exec "$XONOTIC" -userdir $(pwd)/userdir
